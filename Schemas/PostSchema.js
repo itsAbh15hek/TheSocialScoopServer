40a275322e755859bfd9dc346efbb8283bc5a29b
Abh15hek <itsabh15hek@outlook.com>
2022-05-28 15:47:44 +0530

    post type

--- Schemas/PostSchema.js
+++ Schemas/PostSchema.js
@@ -14,6 +14,10 @@ const PostSchema = new mongoose.Schema(
     postMedia: {
       type: String,
     },
+    mediaType: {
+      type: String,
+      default: "",
+    },
     likes: {
       type: Array,
       default: [],
