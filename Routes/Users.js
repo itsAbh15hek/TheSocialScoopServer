2fe10f6e96115578adf2fc0ff01035406e4d6d8c
Abh15hek <itsabh15hek@outlook.com>
2022-05-29 19:46:48 +0530

    profile update blocked by cors issue removed

--- Routes/Users.js
+++ Routes/Users.js
@@ -27,8 +27,7 @@ router.put("/:id", async (req, res) => {
 
     //Updating the user in the database
     try {
-      const user = await User.findByIdAndUpdate(userId, { $set: req.body });
-
+      await User.findByIdAndUpdate(userId, { $set: req.body });
       res.status(200).json("Account has been Updated!");
     } catch (error) {
       //Error Handling
