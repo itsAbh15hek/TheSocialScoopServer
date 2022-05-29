e1de92bffe062a17aed14100a13d5bde6dbcfe20
Abh15hek <itsabh15hek@outlook.com>
2022-05-29 19:19:20 +0530

    Revert "removed the logs form file and made some minor changes"
    This reverts commit c1393791734b4085d7c3b0d378915497b8a4cb16.

--- Routes/Users.js
+++ Routes/Users.js
@@ -7,8 +7,9 @@ const User = require("../Schemas/UserSchema");
 //Update User
 router.put("/:id", async (req, res) => {
   //Destructering the body
+  const { _id: userId, password } = req.body;
   //Checking if user id is present in the database
-  if (req.body._id === req.params.id || req.body.isAdmin) {
+  if (userId === req.params.id || req.body.isAdmin) {
     //Checking if the password is been requested to changed
     if (password) {
       //Updating the password using AES
@@ -26,9 +27,7 @@ router.put("/:id", async (req, res) => {
 
     //Updating the user in the database
     try {
-      const user = await User.findByIdAndUpdate(req.params.id, {
-        $set: req.body,
-      });
+      const user = await User.findByIdAndUpdate(userId, { $set: req.body });
 
       res.status(200).json("Account has been Updated!");
     } catch (error) {
