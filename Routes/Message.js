dfb2bee694f693c49a1fc003bd7d7c43f04cf257
Abh15hek <itsabh15hek@outlook.com>
2022-05-30 21:19:17 +0530

    message encryption working

--- Routes/Message.js
+++ Routes/Message.js
@@ -8,16 +8,16 @@ const Message = require("../Schemas/MessageSchema");
 router.post("/", async (req, res) => {
   //Creating a new Message
   const newMessage = new Message(req.body);
-
   try {
     //Saving the new Message to the database
     const hashedMessage = cryptojs.AES.encrypt(
-      newMessage,
+      newMessage.message,
       process.env.SEC
     ).toString();
 
+    newMessage.message = hashedMessage;
     //Saving the Hashed message to database
-    await hashedMessage.save();
+    await newMessage.save();
 
     //Sending the resposne
     res.status(200).json("Message Sent!");
@@ -34,12 +34,17 @@ router.get("/:conversationId", async (req, res) => {
     const hashedMessages = await Message.find({
       conversationId: req.params.conversationId,
     });
-
     //Decrypting the message
-    const messages = await cryptojs.AES.decrypt(
-      hashedMessages,
-      process.env.SEC
-    ).toString(cryptojs.enc.Utf8);
+    const messages = await Promise.all(
+      hashedMessages.map(async (messageObj) => {
+        const unhashedMessage = await cryptojs.AES.decrypt(
+          messageObj.message,
+          process.env.SEC
+        ).toString(cryptojs.enc.Utf8);
+        messageObj.message = unhashedMessage;
+        return messageObj;
+      })
+    ).then((res) => res);
 
     //Sending back the resposne
     res.status(200).json(messages);
